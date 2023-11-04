aeb6b5a336ca3da53323945cb6faf8b11a2a9c48
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-05 01:33:32 +0300

    challange 2 bugs fixed

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float time = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - time > 1.5)
+        {
+            time = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
